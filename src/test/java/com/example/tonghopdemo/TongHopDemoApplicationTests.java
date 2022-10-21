cd500279325496a9b6f69abbc6f46aaadfba07f4
Bui Phuoc <buiphuoc@Phuocs-Mac.local>
2022-10-21 21:42:43 +0700

    set up git for demos

---
+++ src/test/java/com/example/tonghopdemo/TongHopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tonghopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
