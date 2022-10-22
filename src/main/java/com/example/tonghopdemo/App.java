03316a5622eb2437d8bd38be2d6d22a10048c1a8
Bui Phuoc <buiphuoc@Phuocs-Mac.local>
2022-10-22 12:48:39 +0700

    bổ sung configuration properties xử lý các biến toàn cục

--- src/main/java/com/example/tonghopdemo/App.java
+++ src/main/java/com/example/tonghopdemo/App.java
@@ -1,12 +1,31 @@
 package com.example.tonghopdemo;
 
+import com.example.tonghopdemo.config.BookAppProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class App {
+@EnableConfigurationProperties
+public class App implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+    @Autowired
+    BookAppProperties bookAppProperties;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Global variable:");
+        System.out.println("\t Email: "+bookAppProperties.getDeveloper());
+    }
+    /*
+    Bất kì đâu, khi cần lấy các thông tin config, tôi chỉ cần:
+    @Autowired
+    BookAppProperties bookAppProperties;
+     */
 }
